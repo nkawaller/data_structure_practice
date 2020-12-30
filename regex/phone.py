9cda7fe883cc1bebc00f5d39f5b8e596c6d5b25f
Nathan Kawaller <kawaller@gmail.com>
2020-12-30 08:01:40 -0500

    using pipe character

--- regex/phone.py
+++ regex/phone.py
@@ -1,7 +1,20 @@
 import re
 
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+# phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 
-matchObject = phoneNumRegex.search('My phone number is 773-312-1849.')
+# matchObject = phoneNumRegex.search('My phone number is 773-312-1849.')
 
-print(matchObject.group())
+# print(matchObject.group())
+
+# Regex with groups
+# phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+
+# matchObject = phoneNumRegex.search('My phone number is 773-312-1849.')
+# print(matchObject.groups())
+
+# Multiple matches with pipe
+heroRegex = re.compile(r'Batman|Carl')
+matchObject1 = heroRegex.search('I am watching a shows that star Batman and Carl')
+print(matchObject1.group())
+
+# p.155
