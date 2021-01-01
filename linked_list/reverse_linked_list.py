44ffd1ea3d01cd7528e70c90c6ad96441c02d494
Nathan Kawaller <kawaller@gmail.com>
2021-01-01 14:20:03 -0500

    node and linked list

---
+++ linked_list/reverse_linked_list.py
@@ -0,0 +1,23 @@
+# References https://www.geeksforgeeks.org/reverse-a-linked-list/
+
+# Node class
+
+class Node: 
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
