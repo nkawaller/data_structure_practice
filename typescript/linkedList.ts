1f539be58c5279668a626a082ffcaec4c737127b
Nathan Kawaller <kawaller@gmail.com>
2021-01-11 09:56:25 -0500

    singly linked list

--- typescript/linkedList.ts
+++ typescript/linkedList.ts
@@ -23,3 +23,39 @@ class ListNode {
         this._next = null;
     }
 }
+
+class SinglyLinkedList {
+
+    // Keeps track of the first node of the list
+    private _head: ListNode;
+    public get head(): ListNode {
+        return this._head;
+    }
+    public set head(node: ListNode) {
+        this._head = node;
+    }
+
+    // Keeps track of the last node of the list
+    private _tail: ListNode;
+    public get tail(): ListNode {
+        return this._tail;
+    }
+    public set tail(node: ListNode) {
+        this._tail = node;
+    }
+
+    // Counts the number of nodes in the list
+    private _length: number;
+    public get length(): number {
+        return this._length;
+    }
+    public set length(v: number) {
+        this._length = v;
+    }
+
+    constructor() {
+        this._length = 0;
+        this._head = null;
+        this._tail = null;
+    }
+}
