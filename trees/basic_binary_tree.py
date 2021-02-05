b6bb838ffc6005d63505149139cf4062c84a258a
Nathan Kawaller <kawaller@gmail.com>
2021-02-05 17:43:58 -0500

    driver code

--- trees/basic_binary_tree.py
+++ trees/basic_binary_tree.py
@@ -5,3 +5,19 @@ class Tree:
         self.value = value
         self.left = left
         self.right = right
+
+    def PrintTree(self):
+        print(self.value)
+        if self.left:
+            self.left.PrintTree()
+        if self.right:
+            self.right.PrintTree()
+
+
+r = Tree(8)
+r.left = Tree(3)
+r.right = Tree(10)
+r.left.left = Tree(1)
+r.left.right = Tree(6)
+r.right.right = Tree(14)
+r.PrintTree()
